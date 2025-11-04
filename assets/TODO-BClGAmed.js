eba0a43151a0177ca8f25a5a22cc37ec464bc653
atanaschristov <atanaschristov@users.noreply.github.com>
2025-11-04 11:28:14 +0000

    deploy: 11eafb658727769e195705087f2bcff66732257b

---
+++ assets/TODO-BClGAmed.js
@@ -0,0 +1,13 @@
+const e=`### TODO
+
+Things to do:
+
+- Introduce the concept of root contexts
+- rethink, simplify and refactor the context managers so they have a unified way of processing the commands
+- rethink the way command results are exposed, making sure there is a unified way of handling the commands output
+- rethink simplify and reorganize the localization of the custom external commands. Currently partially implemented.
+
+Things to consider:
+
+- reorganize the code into monorepo
+`;export{e as default};
