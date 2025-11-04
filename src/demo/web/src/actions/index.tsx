71474002af42c107b0cd163ddb268d3a83dc4cdd
Atanas Christov <atanaschristov@gmail.com>
2025-11-04 17:30:53 +0100

    Fix: provides a doc entry action and page, fixes the documentiopn list command

--- src/demo/web/src/actions/index.tsx
+++ src/demo/web/src/actions/index.tsx
@@ -1,12 +1,12 @@
 import { ACTION_NAMES } from '../../../../__mocks__/contexts/';
 // TODO from the library
 import { IActionResponseParameter } from '../../../../lib/types';
-import { List } from '../components/documentation/List';
-import { RenderDocument } from '../components/documentation/RenderDocument';
 
-import DocumentationEntry from '../components/documentation/DocumentationEntry';
-import TodoScreen from '../components/TodoScreen';
-import WelcomeScreen from '../components/WelcomeScreen';
+import DocumentationEntryAction from './actionComponents/documentation/DocumentationEntryAction';
+import ListDocumentsAction from './actionComponents/documentation/ListDocumentsAction';
+import ShowDocumentAction from './actionComponents/documentation/ShowDocumentAction';
+import TodoAction from './actionComponents/lobby/TodoAction';
+import WelcomeScreenAction from './actionComponents/lobby/WelcomeScreenAction';
 
 const docs = import.meta.glob('../../assets/documentation/*.md', { as: 'raw' });
 const todo = import.meta.glob('../../../../../TODO.md', { as: 'raw' });
@@ -16,20 +16,20 @@ export const ACTIONS = {
 		return parameter?.value && <div style={{ whiteSpace: 'pre-wrap' }}>{parameter?.value}</div>;
 	},
 	[ACTION_NAMES.DOCUMENTATION.DOC_ENTRY]: async () => {
-		return <DocumentationEntry />;
+		return <DocumentationEntryAction />;
 	},
 	[ACTION_NAMES.DOCUMENTATION.LIST]: async () => {
 		const paths = Object.keys(docs);
 
-		return <List paths={paths} />;
+		return <ListDocumentsAction paths={paths} />;
 	},
 	[ACTION_NAMES.DOCUMENTATION.SHOW]: async (parameter: unknown) => {
-		return await RenderDocument({ docs, path: (parameter as { value?: string })?.value });
+		return await ShowDocumentAction({ docs, path: (parameter as { value?: string })?.value });
 	},
 	[ACTION_NAMES.LOBBY.LOBBY_WELCOME_SCREEN]: async () => {
-		return <WelcomeScreen />;
+		return <WelcomeScreenAction />;
 	},
 	[ACTION_NAMES.LOBBY.LOBBY_TODO]: async () => {
-		return await TodoScreen({ todoDocument: Object.values(todo)[0] });
+		return await TodoAction({ todoDocument: Object.values(todo)[0] });
 	},
 };
